dc3c556d64415f895b9e7c5566dbfa895adde927
Rakishly <630390925@qq.com>
2020-01-15 17:58:02 +0800

    提交

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -11,12 +11,55 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Auth;
 class UsersController extends Controller
 {
 
+    /**
+     * 用户注册页面
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function create()
+    {
+        return view('users.create');
+    }
+
+    /**
+     * 用户信息页面
+     *
+     * @param User $user
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function show(User $user)
     {
-        dump($user);exit;
         return view('users.show', compact('user'));
+    }
+
+    /**
+     * 注册表单提交
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+        ]);
+        //注册完自动登录
+        Auth::login($user);
+        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
+        return redirect()->route('users.show', [$user->id]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+
+Route::get('/', 'StaticPagesController@home')->name('home');
+Route::get('/help', 'StaticPagesController@help')->name('help');
+Route::get('/about', 'StaticPagesController@about')->name('about');
+
+Route::get('/signup', 'UsersController@create')->name('signup');
+
 Route::resource('users', 'UsersController');
+Route::resource('lawyers', 'LawyersController');
+
+Route::get('/login', 'SessionsController@create')->name('login');
+Route::post('/login', 'SessionsController@store')->name('login');
+Route::delete('/login', 'SessionsController@destroy')->name('logout');
